6efa3a588a3f4e8fdd12072aba9ca751bc147990
ihorl <ihorl@IHORDESKTOP>
2026-04-02 09:10:34 +0300

    Add project files.

---
+++ Test/Models/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Models {
+    public class Author {
+        public int Id { get; set; }
+        public String FullName { get; set; } = String.Empty;
+        public DateTime? BirthDate { get; set; }
+
+        // 
+        public void UpdateProfile(string name, DateTime? birth) {
+            FullName = name;
+            BirthDate = birth;
+        }
+    }
+}
---
+++ Test/Models/Reader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Models {
+    public class Reader {
+        public int Id { get; set; }
+        public string CardNumber { get; set; } = String.Empty;
+        public string FullName { get; set; } = String.Empty;
+        public string Address { get; set; } = String.Empty;
+        public string PhoneNumber { get; set; } = String.Empty;
+
+        // Історія 9: Бібліотекар - Оновлювати інформацію про читача
+        public void UpdateInfo(string name, string addr, string phone) { 
+            if (!string.IsNullOrWhiteSpace(name)) FullName = name;
+            Address = addr;
+            PhoneNumber = phone;
+        }
+    }
+}
