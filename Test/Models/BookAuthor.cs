7da5cd73d00ab4f757faac56fa2e1072c76af7ac
Youngking147 <turchynpavloua@gmail.com>
2026-04-29 23:39:30 +0300

    Add constructors to BookAuthor model

--- Test/Models/BookAuthor.cs
+++ Test/Models/BookAuthor.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,5 +12,13 @@ namespace Test.Models {
         public int AuthorId { get; set; }
         public Author? Author { get; set; }
 
+        public BookAuthor(int bookId, int authorId)
+        {
+            BookId = bookId;
+            AuthorId = authorId;
+        }
+
     }
+
+    
 }
