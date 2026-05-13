a0c7260937ab54933e1b7dafd769d9717805002b
IhorLihvan <85878201+IhorLihvan@users.noreply.github.com>
2026-05-13 23:47:42 +0300

    added untracked files

---
+++ Test/Models/BookLoan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Test.Models {
+    public class BookLoan {
+        [Key]
+        public int BookId { get; set; }
+        public Book? Book { get; set; }
+        public int LoanId { get; set; }
+        public Loan? Loan { get; set; }
+
+        public BookLoan() { }
+
+        public BookLoan(int bookId, int loanId)
+        {
+            BookId = bookId;
+            LoanId = loanId;
+        }
+    }
+}
